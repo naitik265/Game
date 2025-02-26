2e321af8f04b872831254e4c458df50e357bfcda
naitik265 <pg062893@gmail.com>
2025-02-26 13:33:42 +0530

    Update

---
+++ PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private float verticalLookRotation;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Player movement
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * Time.deltaTime);
+
+        // Mouse look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+        verticalLookRotation -= mouseY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(verticalLookRotation, 0, 0);
+    }
+}
